7acb9352dc510bf9f04a05ed83daf6c7ee11dcac
Nick kuo <nickest14@gmail.com>
2022-08-23 22:32:55 +0800

    307. Range Sum Query - Mutable

--- python/easy/Solution_1408.py
+++ python/easy/Solution_1408.py
@@ -2,6 +2,7 @@
 
 from typing import List
 
+
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         ans = set()
@@ -12,5 +13,6 @@ class Solution:
                     break
         return ans
 
-ans = Solution().stringMatching(["mass","as","hero","superhero"])
+
+ans = Solution().stringMatching(["mass", "as", "hero", "superhero"])
 print(ans)
