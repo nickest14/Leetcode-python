4845d04507476cf8b576f672863eb7da1d9d745f
Nick kuo <nickest14@gmail.com>
2022-09-08 23:25:55 +0800

    Leetcode 1380. Lucky Numbers in a Matrix

---
+++ python/easy/Solution_1380.py
@@ -0,0 +1,13 @@
+# 1380. Lucky Numbers in a Matrix
+
+from typing import List
+
+
+class Solution:
+    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+        return list({min(row) for row in matrix}
+                    & {max(col) for col in zip(*matrix)})
+
+
+ans = Solution().luckyNumbers([[3, 7, 8], [9, 11, 13], [15, 16, 17]])
+print(ans)
