3b75f4795e00453f5b07c21f99fbca1aea2337dc
Nick kuo <nickest14@gmail.com>
2022-11-26 10:09:54 +0800

    Refactor leetcode 46 code

--- python/medium/Solution_46.py
+++ python/medium/Solution_46.py
@@ -1,24 +1,42 @@
 # 46. Permutations
 
+from typing import List
+
 
 class Solution:
-    def permutation(self, nums, li):
-        for i in nums:
-            li.append(i)
-            subset = nums.copy()
-            subset.remove(i)
-            if len(subset) == 0:
-                self.ans.append(li.copy())
-            else:
-                self.permutation(subset, li)
-            li.pop()
-
-    def permute(self, nums):
-        self.ans = []
-        li = []
-        self.permutation(nums, li)
-        return self.ans
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        temp = []
+        for num in nums:
+            for sub in result:
+                for i in range(len(sub) + 1):
+                    print(sub[:i] + [num] + sub[i:])
+                    temp.append(sub[:i] + [num] + sub[i:])
+            result = temp
+            temp = []
+        # List comperhension:
+        # result = [sub[:i] + [num] + sub[i:] for sub in result for i in range(len(sub) + 1)]
+        return result
 
 
-ans = Solution().permute([1,2,3])
+# class Solution:
+#     def permutation(self, nums, li):
+#         for i in nums:
+#             li.append(i)
+#             subset = nums.copy()
+#             subset.remove(i)
+#             if len(subset) == 0:
+#                 self.ans.append(li.copy())
+#             else:
+#                 self.permutation(subset, li)
+#             li.pop()
+
+#     def permute(self, nums):
+#         self.ans = []
+#         li = []
+#         self.permutation(nums, li)
+#         return self.ans
+
+
+ans = Solution().permute([1, 2, 3])
 print(ans)
