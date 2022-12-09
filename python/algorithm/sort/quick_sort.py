855e87c0ddadc58614c330f599a4c93825d96ce9
Nick kuo <nickest14@gmail.com>
2022-12-09 22:28:07 +0800

    Add recursive version quick sort

--- python/algorithm/sort/quick_sort.py
+++ python/algorithm/sort/quick_sort.py
@@ -20,6 +20,20 @@ def quick_sort(nums: List, low: int, high: int) -> List:
 
     return nums
 
+# # recursive version, not good
+# def quick_sort(nums):
+#     n = len(nums)
+#     if n <= 1:
+#         return nums
+#     left = []
+#     right = []
+#     pivot = nums[0]
+#     for i in range(1,n):
+#         if nums[i] < pivot:
+#             left.append(nums[i])
+#         else:
+#             right.append(nums[i])
+#     return quick_sort(left) + [pivot] + quick_sort(right)
 
 nums = [1, 5, 10, 2, 20, 3, 8]
 ans = quick_sort(nums, 0, len(nums) - 1)
