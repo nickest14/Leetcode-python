378cb1b82d72bcbb472655be933750923e1ab8a5
Nick kuo <nickest14@gmail.com>
2023-03-21 17:14:42 +0800

    Add bfs solution with leetcode 785

--- python/medium/Solution_785.py
+++ python/medium/Solution_785.py
@@ -14,6 +14,27 @@ class Solution:
         colors = {}
         return all(dfs(u, 1) for u in range(len(graph)) if u not in colors)
 
+    # # bfs
+    # def isBipartite(self, graph: List[List[int]]) -> bool:
+    #     import collections
+    #     colors = {}
+    #     for i in range(len(graph)):
+    #         if i in colors:
+    #             continue
+    #         color = 1
+    #         queue = collections.deque([i])
+    #         while queue:
+    #             for _ in range(len(queue)):
+    #                 node = queue.popleft()
+    #                 if node in colors:
+    #                     if colors[node] != color:
+    #                         return False
+    #                 else:
+    #                     colors[node] = color
+    #                     queue += graph[node]
+    #             color ^= 1
+    #     return True
+
 
 graph = [[1, 2, 3], [0, 2], [0, 1, 3], [0, 2]]
 # graph = [[1, 3], [0, 2], [1, 3], [0, 2]]
