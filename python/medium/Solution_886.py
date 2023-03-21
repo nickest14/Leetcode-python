be69e61c2b69cff621a131f167f5a30ab716cbb3
Nick kuo <nickest14@gmail.com>
2023-03-21 17:26:05 +0800

    Add bfs solution with leetcode 886

--- python/medium/Solution_886.py
+++ python/medium/Solution_886.py
@@ -26,6 +26,29 @@ class Solution:
                 check_nodes.append(dfs(node, 0))
         return all(check_nodes)
 
+    # # bfs
+    # def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+    #     graph, colors = collections.defaultdict(list), {}
+    #     for u, v in dislikes:
+    #         graph[u].append(v)
+    #         graph[v].append(u)
+    #     for i in range(1, n + 1):
+    #         if i in colors:
+    #             continue
+    #         color = 1
+    #         queue = collections.deque([i])
+    #         while queue:
+    #             for _ in range(len(queue)):
+    #                 node = queue.popleft()
+    #                 if node in colors:
+    #                     if colors[node] != color:
+    #                         return False
+    #                 else:
+    #                     colors[node] = color
+    #                     queue += graph[node]
+    #             color ^= 1
+    #     return True
+
 
 n = 5
 dislikes = [[1, 2], [1, 3], [2, 4], [2, 5]]
