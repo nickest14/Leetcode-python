c5b0b77cdd803c25cfe32da043d3da4f2c837a00
Nick kuo <nickest14@gmail.com>
2023-05-22 23:56:00 +0800

    Refactor leetcode 8

--- python/medium/Solution_8.py
+++ python/medium/Solution_8.py
@@ -3,41 +3,19 @@
 
 class Solution:
     def myAtoi(self, str: str) -> int:
-        ans = 0
-        length = len(str)
-        if length == 0:
-            pass
-        elif length == 1:
-            ans = int(str) if str.isnumeric() else 0
-        else:
-            i = 0
-            negative = False
-            while i < length:
-                if str[i] == ' ':
-                    i += 1
-                elif str[i] == '-':
-                    negative = True
-                    i += 1
-                    break
-                elif str[i] == '+':
-                    i += 1
-                    break
-                else:
-                    break
+        s = str.lstrip()
+        sign = 1
+        if s and s[0] in '+-':
+            if s[0] == '-':
+                sign = -1
+            s = s[1:]
+        num = 0
+        for c in s:
+            if not c.isdigit():
+                break
+            num = 10 * num + int(c)
+        return max(-2**31, min(2**31 - 1, sign * num))
 
-            while i < length:
-                if str[i].isnumeric():
-                    ans = ans * 10 + int(str[i])
-                    i += 1
-                else:
-                    break
-            if negative:
-                ans *= -1
-            if ans > 2**31 - 1:
-                ans = 2**31 - 1
-            elif ans < -2**31:
-                ans = -2**31
-        return ans
 
-ans = Solution().myAtoi('  +998AAA656_J)(')
+ans = Solution().myAtoi('  -99ABC')
 print(ans)
