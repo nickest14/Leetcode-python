c2bfb0ff5f5728400330a2ac6293ad2cf836c38c
Nick kuo <nickest14@gmail.com>
2023-07-04 23:55:29 +0800

    Add another solution for leetcode 15

--- python/medium/Solution_15.py
+++ python/medium/Solution_15.py
@@ -1,7 +1,34 @@
 # 15. 3Sum
 
+from typing import List
+
 
 class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for i, val in enumerate(nums):
+            if val > 0:
+                break
+            if i > 0 and val == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = val + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
+                else:
+                    ans.append([val, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+        return ans
+
+
+class Solution2:
     def threeSum(self, nums):
         ans = []
         if len(nums) < 3:
@@ -45,8 +72,7 @@ class Solution:
 
 
 # ans = Solution().threeSum([-1, 1, -1, -1, -1, -1, 2, 5, 0, 2, 2, 2, 6])
-ans = Solution().threeSum([1, 1, 1])
 # ans = Solution().threeSum([0, 0, 0, 0, 0, 0, 0, -1])
 
-# ans = Solution().threeSum([-1, 0, 1, 2, -1, -4])
+ans = Solution().threeSum([-1, 0, 1, 2, -1, -4])
 print(ans)
