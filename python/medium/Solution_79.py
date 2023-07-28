030baca5f7cf9da6783b9806473d81c47562517f
Nick kuo <nickest14@gmail.com>
2023-07-28 21:11:06 +0800

    Refactor leetcode 79. Word Search

--- python/medium/Solution_79.py
+++ python/medium/Solution_79.py
@@ -1,6 +1,31 @@
 # 79. Word Search
 
+from typing import List
+
+
 class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        path = set()
+
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if (min(r, c) < 0 or r >= rows or c >= cols or word[i] != board[r][c] or (r, c) in path):
+                return False
+            path.add((r, c))
+            ans = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            path.remove((r, c))
+            return ans
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
+                    return True
+        return False
+
+
+class Solution2:
     def exist(self, board, word: str) -> bool:
         if not word:
             return False
@@ -51,15 +76,9 @@ class Solution:
 
 
 board = [
-    ['A','B','C','E'], 
-    ['S','F','C','S'], 
-    ['A','D','E','E']
-]
-
-board = [
-    [], 
-    [], 
-    []
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E']
 ]
 
 word = 'ASD'
