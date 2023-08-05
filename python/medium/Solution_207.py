529117d5fa68ea98851e81880431d3c0aafc3ad2
Nick kuo <nickest14@gmail.com>
2023-08-05 15:08:00 +0800

    Refactor leetcode 207. Course Schedule

--- python/medium/Solution_207.py
+++ python/medium/Solution_207.py
@@ -5,6 +5,33 @@ from typing import List
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # dfs
+        pre_dict = {i: [] for i in range(numCourses)}
+        for cur, pre in prerequisites:
+            pre_dict[cur].append(pre)
+        visiting = set()
+
+        def dfs(cur):
+            if cur in visiting:
+                return False
+            if not pre_dict[cur]:
+                return True
+            visiting.add(cur)
+            for pre in pre_dict[cur]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(cur)
+            pre_dict[cur] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
+
+
+class Solution2:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def dfs(vertex: int) -> bool:
             if status[vertex] > -1:
                 return status[vertex]
@@ -26,6 +53,7 @@ class Solution:
 
 
 numCourses = 5
-prerequisites = [[0, 1], [0, 2], [0, 3], [3, 2], [4, 0], [1, 4]]
+prerequisites = [[0, 1], [0, 2], [0, 3], [3, 2], [0, 4], [1, 4]]
+# prerequisites = [[0, 1], [0, 2], [0, 3], [3, 2], [4, 0], [1, 4]]
 ans = Solution().canFinish(numCourses, prerequisites)
 print(ans)
