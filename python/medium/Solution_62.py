c7034cae6c315deca3f0b0eb17b685c0d08c8b93
Nick kuo <nickest14@gmail.com>
2023-08-17 21:07:08 +0800

    Refactor Leetcode 62. Unique Paths

--- python/medium/Solution_62.py
+++ python/medium/Solution_62.py
@@ -3,30 +3,15 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        array = [[0 for i in range(n+1)] for j in range(m+1)]
-        print(array)
+        row = [1] * n
 
-        def getorupdate(m, n):
-            if array[m][n] == 0:
-                array[m][n] = dfs(m, n)
-            return array[m][n]
-
-        def dfs(m, n):
-            if m == 1 and n == 1:
-                return 1
-            elif m < 1 or n < 1:
-                return 0
-            return getorupdate(m - 1, n) + getorupdate(m, n - 1)
-        return dfs(m, n)
-
-    # USe dfs, but time limit exceeded
-    def uniquePaths_dfs(self, m: int, n: int) -> int:
-        if m == 1 and n == 1:
-            return 1
-        elif m < 1 or n < 1:
-            return 0
-        return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
+        for _ in range(m - 1):
+            new_row = [1] * n
+            for j in range(1, n,):
+                new_row[j] = new_row[j - 1] + row[j]
+            row = new_row
+        return row[-1]
 
 
-ans = Solution().uniquePaths(5, 2)
+ans = Solution().uniquePaths(3, 5)
 print(ans)
