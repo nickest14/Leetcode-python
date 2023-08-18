fa473a3cb21841a174622cdf90719b9ccb43b7e2
Nick kuo <nickest14@gmail.com>
2023-08-18 21:29:29 +0800

    Refactor leetcode 1143. Longest Common Subsequence

--- python/medium/Solution_1143.py
+++ python/medium/Solution_1143.py
@@ -3,18 +3,28 @@
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        matrix = [[0 for _ in range(len(text2))] for _ in range(len(text1))]
-        for i in range(len(text1)):
-            for j in range(len(text2)):
+        dp = [[0 for j in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+        for i in range(len(text1) - 1, -1, -1):
+            for j in range(len(text2) - 1, -1, -1):
                 if text1[i] == text2[j]:
-                    matrix[i][j] = 1 + (matrix[i - 1][j - 1] if i >= 1 and j >= 1 else 0)
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
                 else:
-                    left = matrix[i - 1][j] if i >= 1 else 0
-                    right = matrix[i][j - 1] if j >= 1 else 0
-                    matrix[i][j] = max(left, right)
-        return matrix[len(text1) - 1][len(text2) - 1]
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+        return dp[0][0]
+
+    # def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    #     matrix = [[0 for _ in range(len(text2))] for _ in range(len(text1))]
+    #     for i in range(len(text1)):
+    #         for j in range(len(text2)):
+    #             if text1[i] == text2[j]:
+    #                 matrix[i][j] = 1 + (matrix[i - 1][j - 1] if i >= 1 and j >= 1 else 0)
+    #             else:
+    #                 left = matrix[i - 1][j] if i >= 1 else 0
+    #                 right = matrix[i][j - 1] if j >= 1 else 0
+    #                 matrix[i][j] = max(left, right)
+    #     return matrix[len(text1) - 1][len(text2) - 1]
 
 
-# ans = Solution().longestCommonSubsequence('abcde', 'ace')
-ans = Solution().longestCommonSubsequence('oxcpqrsvwf', 'shmtulqrypy')
+ans = Solution().longestCommonSubsequence('abcde', 'ace')
+# ans = Solution().longestCommonSubsequence('oxcpqrsvwf', 'shmtulqrypy')
 print(ans)
