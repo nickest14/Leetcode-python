435deadfe2b3936dd7848a384d2d9a364352268c
Nick kuo <nickest14@gmail.com>
2023-08-30 21:52:10 +0800

    Refactor leetcode 268

--- python/easy/Solution_268.py
+++ python/easy/Solution_268.py
@@ -5,8 +5,16 @@ from typing import List
 
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        length = len(nums)
-        return (length + 1) * length // 2 - sum(nums)
+        ans = len(nums)
+
+        for i in range(len(nums)):
+            ans += i - nums[i]
+        return ans
+
+    # Another solutionans
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     length = len(nums)
+    #     return (length + 1) * length // 2 - sum(nums)
 
 
 ans = Solution().missingNumber([1, 3, 5, 2, 0])
