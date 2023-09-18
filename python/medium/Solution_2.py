d518851209b18cc0ef439c5b04272bced67f0119
Nick kuo <nickest14@gmail.com>
2023-09-18 10:10:38 +0800

    Refactor leetcode 2. Add Two Numbers

--- python/medium/Solution_2.py
+++ python/medium/Solution_2.py
@@ -1,41 +1,37 @@
 # 2. Add Two Numbers
 
+from typing import Optional
+
 
 # Definition for singly-linked list.
 class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not (l1 or l2):
-            return []
-        index, total_val = 0, 0
-        l1_flag, l2_flag = False, False
-        if l1:
-            l1_flag = True
-            total_val += l1.val
-        if l2:
-            l2_flag = True
-            total_val += l2.val
-        while l1_flag or l2_flag:
-            index += 1
-            if l1_flag:
-                if l1 and l1.next:
-                    l1 = l1.next
-                    total_val += l1.val * 10**index
-                else:
-                    l1_flag = False
-            if l2_flag:
-                if l2 and l2.next:
-                    l2 = l2.next
-                    total_val += l2.val * 10**index
-                else:
-                    l2_flag = False
-        ans = [int(i) for i in str(total_val)[::-1]]
-        return ans
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+
+            # new digits
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update pointers
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return dummy.next
 
 
 l1 = ListNode(2)
@@ -49,5 +45,7 @@ l2_1 = ListNode(6)
 l2_2 = ListNode(4)
 l2.next = l2_1
 l2_1.next = l2_2
-ans = Solution().addTwoNumbers(l1, l2)
-print(ans)
+node = Solution().addTwoNumbers(l1, l2)
+while node:
+    print(node.val)
+    node = node.next
