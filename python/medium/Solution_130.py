0e2970d3a3367dba8b31d84675315981c5cab593
Nick kuo <nickest14@gmail.com>
2023-09-21 20:56:47 +0800

    Refactor leetcode 130. Surrounded Regions

--- python/medium/Solution_130.py
+++ python/medium/Solution_130.py
@@ -1,69 +1,41 @@
 # 130. Surrounded Regions
 
+from typing import List
+
+
 class Solution:
 
-    def solve(self, board) -> None:
+    def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        x_length = len(board)
-        if x_length == 0: 
-            return
+        rows, cols = len(board), len(board[0])
 
-        y_length = len(board[0])
-        confirmed = set()
-        dfs = []
-        for i in range(x_length):
-            if board[i][0] == 'O':
-                board[i][0] = 'temp'
-                dfs.append((i, 0))
-            if board[i][y_length - 1] == 'O':
-                board[i][y_length - 1] = 'temp'
-                dfs.append((i, y_length - 1))
-        for j in range(y_length):
-            if board[0][j] == 'O':
-                board[0][j] = 'temp'
-                dfs.append((0, j))
-            if board[x_length - 1][j] == 'O':
-                board[x_length - 1][j] = 'temp'
-                dfs.append((x_length - 1, j))
-        while dfs:
-            i, j = dfs.pop()
-            confirmed.add((i, j))
-            if i+1 < x_length and board[i+1][j] == 'O':
-                board[i+1][j] = 'temp'
-                dfs.append((i + 1, j))
-            if i > 0 and board[i-1][j] == 'O':
-                board[i-1][j] = 'temp'
-                dfs.append((i-1, j))
-            if j+1 < y_length and board[i][j+1] == 'O':
-                board[i][j+1] = 'temp'
-                dfs.append((i, j + 1))
-            if j > 0 and board[i][j-1] == 'O':
-                board[i][j-1] = 'temp'
-                dfs.append((i, j-1))
-        for i in range(x_length):
-            for j in range(y_length):
-                if (i, j) in confirmed:
-                    board[i][j] = 'O'
-                else:
-                    board[i][j] = 'X'
-        return
+        def capture(r: int, c: int):
+            if r < 0 or c < 0 or r == rows or c == cols or board[r][c] != 'O':
+                return
+            board[r][c] = 'T'
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
 
-arr = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
-arr2 = [["X","O","X","X"],["X","O","X","X"],["X","O","O","X"],["X","X","O","X"]]
-arr3 = [
-    ["O","X","O","O","O","O","O","O","O"],
-    ["O","O","O","X","O","O","O","O","X"],
-    ["O","X","O","X","O","O","O","O","X"],
-    ["O","O","O","O","X","O","O","O","O"],
-    ["X","O","O","O","O","O","O","O","X"],
-    ["X","X","O","O","X","O","X","O","X"],
-    ["O","O","O","X","O","O","O","O","O"],
-    ["O","O","O","X","O","O","O","O","O"],
-    ["O","O","O","O","O","X","X","O","O"]]
-ans = Solution().solve(arr2)
-for i in ans:
-    print(i)
-# print(ans)
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and (r in [0, rows - 1] or c in [0, cols - 1]):
+                    capture(r, c)
 
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'T':
+                    board[r][c] = 'O'
+
+
+arr = [["X", "X", "X", "X"], ["X", "O", "O", "X"], ["X", "O", "O", "X"], ["X", "O", "X", "X"]]
+Solution().solve(arr)
+print(arr)
