320537e1e768843751ba3f1e7e1d0927fb643b32
Nick kuo <nickest14@gmail.com>
2023-09-22 22:13:06 +0800

    Refactor leetcode 88. Merge Sorted Array

--- python/easy/Solution_88.py
+++ python/easy/Solution_88.py
@@ -1,27 +1,23 @@
 # 88. Merge Sorted Array
 
+from typing import List
+
 
 class Solution:
-    def merge(self, nums1, m, nums2, n):
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        num1_index = 0
-        length = len(nums1)
-        for _ in range(length-m):
-            nums1.pop()
-        for i in range(n):
-            while num1_index < length:
-                try:
-                    if nums1[num1_index] >= nums2[i]:
-                        nums1.insert(num1_index, nums2[i])
-                        break
-                except:
-                    nums1.append(nums2[i])
-                    break
-                num1_index += 1
-        print(nums1)
+        while m > 0 and n > 0:
+            if nums1[m - 1] >= nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
 
 
-# Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
-Solution().merge([-1, 0, 0, 3, 3, 3, 0, 0, 0], 6, [1, 2, 2], 3)
+Solution().merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
+# Solution().merge([7, 8, 9, 0, 0, 0], 3, [2, 5, 6], 3)
