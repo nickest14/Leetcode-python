26c144caa5226ee2534e96667f2d0a104b6d0d1d
Nick kuo <nickest14@gmail.com>
2023-10-30 16:43:58 +0800

    Leetcode 343. Integer Break

---
+++ python/medium/Solution_343.py
@@ -0,0 +1,15 @@
+# 343. Integer Break
+
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = {1: 1}
+        for num in range(2, n + 1):
+            dp[num] = 0 if num == n else num
+            for i in range(1, num):
+                val = dp[i] * dp[num - i]
+                dp[num] = max(dp[num], val)
+        return dp[n]
+
+
+ans = Solution().integerBreak(5)
+print(ans)
