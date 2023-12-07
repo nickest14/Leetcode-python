b2b42bf387e84c8c4855ce7527fa3e0a22797346
Nick kuo <nickest14@gmail.com>
2023-12-07 21:48:58 +0800

    Refactor leetcode 118. Pascal's Triangle

--- python/easy/Solution_118.py
+++ python/easy/Solution_118.py
@@ -1,18 +1,20 @@
 # 118. Pascal's Triangle
 
+from typing import List
+
+
 class Solution:
-    def generate(self, numRows: int):
-        ans = []
-        for x in range(numRows):
-            temp_li = []
-            for y in range(x+1):
-                if y == 0 or y == x:
-                    value = 1
-                else:
-                    value = ans[x-1][y-1] + ans[x-1][y]
-                temp_li.append(value)
-            ans.append(temp_li)
+    def generate(self, numRows: int) -> List[List[int]]:
+        ans = [[1]]  # layer 1
+        for layer in range(2, numRows + 1):  # layer 2 to n
+            tmp = [1]
+            for i in range(1, layer - 1):
+                tmp.append(ans[-1][i] + ans[-1][i - 1])
+            tmp.append(1)
+            ans.append(tmp)
+
         return ans
+
 
 # [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 ans = Solution().generate(5)
