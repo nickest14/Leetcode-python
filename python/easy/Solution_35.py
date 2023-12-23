17a9774d7c93f32bb1c5274d5bcf3862a09e55a4
Nick kuo <nickest14@gmail.com>
2023-12-23 14:45:46 +0800

    Refactor leetcode 35. Search Insert Position

--- python/easy/Solution_35.py
+++ python/easy/Solution_35.py
@@ -3,10 +3,14 @@
 
 class Solution:
     def searchInsert(self, nums, target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] >= target:
-                return i
-        return i+1
+        low, high = 0, len(nums)
+        while low < high:
+            mid = low + ((high - low) // 2)
+            if target > nums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
 
 
 ans = Solution().searchInsert([1, 3, 5, 6], 5)
