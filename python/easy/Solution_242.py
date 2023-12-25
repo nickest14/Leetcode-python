671403d0bf8d29f9c7467f452ab1f83d9a8ecda9
Nick kuo <nickest14@gmail.com>
2023-12-26 00:03:56 +0800

    Refactor leetcode 242. Valid Anagram

--- python/easy/Solution_242.py
+++ python/easy/Solution_242.py
@@ -1,11 +1,18 @@
 # 242. Valid Anagram
 
-import collections
-
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return collections.Counter(s) == collections.Counter(t)
+        if len(s) != len(t):
+            return False
+        count_s, count_t = {}, {}
+        for i in range(len(s)):
+            count_s[s[i]] = count_s.get(s[i], 0) + 1
+            count_t[t[i]] = count_t.get(t[i], 0) + 1
+        return count_s == count_t
+
+    # Or just use collections.Counter()
+    # import collections
+    # return collections.Counter(s) == collections.Counter(t)
 
 
 ans = Solution().isAnagram('abcd', 'dcba')
