2afce4948adcb0ff14c7d6b560902089a32549f9
Nick kuo <nickest14@gmail.com>
2024-01-04 21:02:53 +0800

    Refactor leetcode 14. Longest Common Prefix

--- python/easy/Solution_14.py
+++ python/easy/Solution_14.py
@@ -5,12 +5,13 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        last = '' if not strs else strs.pop()
-        for i, v in enumerate(last):
-            for str in strs:
-                if i >= len(str) or str[i] != v:
-                    return last[:i]
-        return last
+        ans = ''
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return ans
+            ans += strs[0][i]
+        return ans
 
 
 strs = ["flower", "flow", "flight"]
