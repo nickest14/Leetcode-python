1499e52c2391325ef1f8a75daf0e926ada13d5de
Nick <nickest14@gmail.com>
2024-01-23 20:45:16 +0800

    Leetcode 1930. Unique Length-3 Palindromic Subsequences

---
+++ python/medium/Solution_1930.py
@@ -0,0 +1,15 @@
+# 1930. Unique Length-3 Palindromic Subsequences
+
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        ans = 0
+        uniq = set(s)
+        for c in uniq:
+            start, end = s.find(c), s.rfind(c)
+            if start < end:
+                ans += len(set(s[start + 1: end]))
+        return ans
+
+
+ans = Solution().countPalindromicSubsequence('ababcab')
+print(ans)
